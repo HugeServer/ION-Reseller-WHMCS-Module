ebf4e241a48cd158b3ed0c5c2a67ffea856b8e5b
Saman Soltani <mr.s4m4n@gmail.com>
2015-08-22 18:40:25 +0430

    Update reboot.php

--- reboot.php
+++ reboot.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * WHMCS v6 Module for HugeServer Resellers
+ *
+ * @author   HugeServer Networks, LLC - Development Team
+ */
+ 
    include "config.php";
    require_once __DIR__ . "/functions.php";
    include "api.php";
