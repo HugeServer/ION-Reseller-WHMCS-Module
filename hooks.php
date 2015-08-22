f2ee5ed508b2989f5015dc0ad66f11032e96e8f6
Saman Soltani <mr.s4m4n@gmail.com>
2015-08-22 18:40:12 +0430

    Update hooks.php

--- hooks.php
+++ hooks.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * WHMCS v6 Module for HugeServer Resellers
+ *
+ * @author   HugeServer Networks, LLC - Development Team
+ */
+ 
 include_once "config.php";
 include_once "functions.php";
 include_once "api.php";
